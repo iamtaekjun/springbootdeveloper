f0c6f609933b65f4a9c85ad27014eeebdcd923c2
iamtaekjun <jtj72272503@gmail.com>
2025-07-13 19:38:18 +0900

    code update

--- src/main/java/me/iamtaekjun/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/iamtaekjun/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -2,8 +2,12 @@ package me.iamtaekjun.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("me.iamtaekjun.springbootdeveloper.domain")
+@EnableJpaRepositories("me.iamtaekjun.springbootdeveloper.repository")
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
