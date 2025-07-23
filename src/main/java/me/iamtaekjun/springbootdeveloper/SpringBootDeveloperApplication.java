bde4f91f5efc3a3f60aca9da9c51adc166736daa
iamtaekjun <jtj72272503@gmail.com>
2025-07-23 18:23:24 +0900

    code update

--- src/main/java/me/iamtaekjun/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/iamtaekjun/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,8 +3,10 @@ package me.iamtaekjun.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing // created_at, updated_at 자동 업데이트
 @SpringBootApplication
 @EntityScan("me.iamtaekjun.springbootdeveloper.domain")
 @EnableJpaRepositories("me.iamtaekjun.springbootdeveloper.repository")
