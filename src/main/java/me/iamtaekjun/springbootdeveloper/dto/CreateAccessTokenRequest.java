514d20b9e49c6191ba10de702ac4a2e8a75acd50
TaekjunJeong <jtj72272503@gmail.com>
2025-09-09 21:16:47 +0900

    CU: JWT

---
+++ src/main/java/me/iamtaekjun/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,10 @@
+package me.iamtaekjun.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
